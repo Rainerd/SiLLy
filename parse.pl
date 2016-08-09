5719ef6b23ab47db38daf1f2f5c72dfd488aa2d0
Rainer Blome <Rainer.Blome@gmx.de>
2016-08-09 23:27:30 +0200

    parse.pl Cleanup: Renamed `LOCATION_MATCH` to `LOCATION_REASON`,
    to support other reasons than matches for creating location objects.

--- parse.pl
+++ parse.pl
@@ -975,7 +975,7 @@ sub grammar_objects($$)
 use constant LOCATION_POS   => 0;
 use constant LOCATION_LINENO => 1 + LOCATION_POS;
 use constant LOCATION_LINE_START => 1 + LOCATION_LINENO;
-use constant LOCATION_MATCH => 1 + LOCATION_LINE_START;
+use constant LOCATION_REASON => 1 + LOCATION_LINE_START;
 
 # --------------------------------------------------------------------
 sub format_stashed($)
@@ -996,7 +996,7 @@ sub format_stashed($)
               .($stashed->[LOCATION_POS]-$stashed->[LOCATION_LINE_START]+1).": "
               : "")
             ."[$$stashed[LOCATION_POS]]"
-            . Parse::SiLLy::Result::toString($$stashed[LOCATION_MATCH], " ")
+            . Parse::SiLLy::Result::toString($$stashed[LOCATION_REASON], " ")
             ;
     }
     else {
@@ -2357,7 +2357,7 @@ sub match_with_memoize($$)
             if (DEBUG() && $log->is_debug()) {
                 $log->debug("Pos after jump: ".get_pos($state));
             }
-            return ($$stashed[LOCATION_MATCH]);
+            return ($$stashed[LOCATION_REASON]);
         }
         else {
             return $stashed;
